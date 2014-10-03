c8f654636b028005c9d4b11fd0d1247c2fb16921
Kali Kiger <kalikiger@gmail.com>
2014-10-03 15:54:50 -0700

    one way to do it

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var myArray = ['a', 'b','c', 'd'];
 
 var myReverse = function(){
-  console.log(myArray);
-  console.log(myArray.length);
+  var aNow = myArray[0]
+  var bNow = myArray[1]
+  var cNow = myArray[2]
+  var dNow = myArray[3]
+  var newArray = [dNow, cNow, bNow, aNow,];
+  console.log(newArray);
 };
 
-myReverse(myArray);
+myReverse();
--- scratchpaper.js
+++ scratchpaper.js
@@ -3,6 +3,17 @@ var myArray = ['a', 'b','c', 'd'];
 var myReverse = function(){
   console.log(myArray);
   console.log(myArray.length);
+};
+
+myReverse(myArray);
+
+
+
+var myArray = ['a', 'b','c', 'd'];
+
+var myReverse = function(){
+  console.log(myArray);
+  console.log(myArray.length);
   newA = myArray.length - 1
   // newB = myArray.length - 2
   // newC = myArray.length - 3
@@ -12,6 +23,21 @@ var myReverse = function(){
   // console.log(newC);
   // console.log(newD);
 
+};
+
+myReverse(myArray);
+
+var myArray = ['a', 'b','c', 'd'];
+
+var myReverse = function(){
+  console.log(myArray);
+  console.log(myArray.length);
+  var aNow = myArray[0]
+  var bNow = myArray[1]
+  var cNow = myArray[2]
+  var dNow = myArray[3]
+  var newArray = [dNow, cNow, bNow, aNow,];
+  console.log(newArray);
 };
 
 myReverse(myArray);
