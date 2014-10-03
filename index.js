a8a14d2a4e02b41b31a81b2fb7a2f6943eb9a322
Kali Kiger <kalikiger@gmail.com>
2014-10-03 15:44:06 -0700

    started console logging

---
+++ index.js
@@ -0,0 +1,8 @@
+var myArray = ['a', 'b','c', 'd'];
+
+var myReverse = function(){
+  console.log(myArray);
+  console.log(myArray.length);
+};
+
+myReverse(myArray);
---
+++ scratchpaper.js
@@ -0,0 +1,17 @@
+var myArray = ['a', 'b','c', 'd'];
+
+var myReverse = function(){
+  console.log(myArray);
+  console.log(myArray.length);
+  newA = myArray.length - 1
+  // newB = myArray.length - 2
+  // newC = myArray.length - 3
+  // newD = myArray.length - 4
+  console.log(newA);
+  // console.log(newB);
+  // console.log(newC);
+  // console.log(newD);
+
+};
+
+myReverse(myArray);
